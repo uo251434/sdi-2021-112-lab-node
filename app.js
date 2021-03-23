5197b413749f6299362b909730486c8ea7648d28
Taso <uo251434@uniovi.es>
2021-03-23 17:38:00 +0100

    “SDI-2021-112-7.2-Express, depuración y variables de entorno.

---
+++ app.js
@@ -0,0 +1,21 @@
+//Modulos
+let express = require('express');
+let app = express();
+
+// Variables
+app.set('port', 8081);
+
+
+app.get('/usuarios', function(req,res) {
+    console.log("depurar aqui");
+    res.send('ver usuarios');
+})
+
+app.get('/canciones', function(req,res){
+    res.send('ver caciones');
+});
+
+//lanzar el serviddor
+app.listen(app.get('port'), function() {
+    console.log('Servidor activo');
+})
