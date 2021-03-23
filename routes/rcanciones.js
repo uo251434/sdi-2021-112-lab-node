de8364082c1fd5fd6818c69e8ec419309d51e4af
Taso <uo251434@uniovi.es>
2021-03-23 21:38:53 +0100

    SDI-2021-112-7.5-Vistas y motores de plantillas.”

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,14 +1,33 @@
-module.exports = function(app) {
+module.exports = function(app, swig) {
+
+
+    app.get('/canciones/agregar', function (req, res) {
+        let respuesta = swig.renderFile('views/bagregar.html', {
+
+        });
+        res.send(respuesta);
+    })
+
+
+
     app.get("/canciones", function(req, res) {
-        let respuesta = "";
-
-        if(req.query.nombre != null)
-            respuesta += 'Nombre: ' + req.query.nombre + '<br>'
-
-        if(req.query.author != null)
-            respuesta += 'Nombre: ' + req.query.author + '<br>'
 
 
+        let canciones = [ {
+            "nombre" : "Blank space",
+            "precio" : "1.2"
+        },{
+            "nombre" : "See you again",
+            "precio" : "1.3"
+        },{
+            "nombre" : "Upwoen Funk",
+            "precio" : "1.1"
+        }];
+
+        let respuesta =  swig.renderFile('views/btienda.html', {
+            vendedor : 'Tienda de canciones',
+            canciones : canciones
+        });
 
 
         res.send(respuesta);
