4165464eed4625404dd1c792d106710cf73da339
Taso <uo251434@uniovi.es>
2021-04-04 21:33:53 +0200

    SDI-2021-112-8.4-Registro de usuario, autenticación y uso de sesión.

--- app.js
+++ app.js
@@ -2,6 +2,15 @@
 let express = require('express');
 let app = express();
 
+let expressSession = require('express-session');
+app.use(expressSession({
+    secret: 'abcdefg',
+    resave: true,
+    saveUninitialized: true
+}));
+
+let crypto = require('crypto');
+
 let fileUpload = require('express-fileupload');
 app.use(fileUpload());
 
@@ -26,7 +35,10 @@ app.set('port', 8081);
 app.set('db', 'mongodb://admin:sdi@tiendamusica-shard-00-00.7ya3n.mongodb.net:27017,' +
     'tiendamusica-shard-00-01.7ya3n.mongodb.net:27017,' +
     'tiendamusica-shard-00-02.7ya3n.mongodb.net:27017/' +
-    'myFirstDatabase?ssl=true&replicaSet=atlas-e7np4x-shard-0&authSource=admin&retryWrites=true&w=majority\n')
+    'myFirstDatabase?ssl=true&replicaSet=atlas-e7np4x-shard-0&authSource=admin&retryWrites=true&w=majority')
+
+app.set('clave','abcdefg');
+app.set('crypto',crypto);
 
 //Rutas/controladores por lógica
 require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -2,9 +2,12 @@
 module.exports = function(app, swig, gestorBD) {
 
     app.get('/canciones/agregar', function (req, res) {
-        let respuesta = swig.renderFile('views/bagregar.html', {
+        if ( req.session.usuario == null){
+            res.redirect("/tienda");
+            return;
+        }
 
-        });
+        let respuesta = swig.renderFile('views/bagregar.html', {});
         res.send(respuesta);
     })
 
@@ -82,10 +85,16 @@ module.exports = function(app, swig, gestorBD) {
 
 
     app.post("/cancion", function(req, res) {
+        if ( req.session.usuario == null){
+            res.redirect("/tienda");
+            return;
+        }
+
         let cancion = {
             nombre : req.body.nombre,
             genero : req.body.genero,
-            precio : req.body.precio
+            precio : req.body.precio,
+            autor: req.session.usuario
         }
 
         // Conectarse
